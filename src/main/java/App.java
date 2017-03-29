cb91ded351e5109f5b0259f53c2740e0bcef4768
Clifford Grimmell and Taylor Loftis-Kim <taylor@>
2017-03-29 16:38:47 -0700

    currently not working

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,23 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/add-user", (request response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("users", User.all());
+      model.put("template", "templates/users.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/add-user", (request response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      // User user = User.find(request.queryParams("userName"));
+      String userName = request.queryParams("userName");
+      String userBio = request.queryParams("userBio");
+      User newUser = new User(userName, userBio);
+      User.save(newUser);
+      model.put("template", "templates/user-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
